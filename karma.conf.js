b2e5dad6687a3f51e06993e32b0e20d97248e303
Jesús Germade <jesus@germade.es>
2014-12-13 17:00:16 +0100

    increasing version

--- karma.conf.js
+++ karma.conf.js
@@ -18,7 +18,7 @@ module.exports = function(config) {
   };
 
   if(process.env.TRAVIS){
-    configuration.browsers = ['Chrome_travis_ci'];
+    configuration.browsers = ['Chrome_travis_ci', 'Firefox'];
   }
 
   config.set(configuration);
